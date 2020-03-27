6f73f4127c92b92c4572c14740f3558cf10eb3ea
w. Patrick Gale <w.patrick.gale@unc.edu>
2020-03-26 20:11:53 -0400

    cache test2

--- rollup.config.js
+++ rollup.config.js
@@ -9,28 +9,6 @@ import copy from 'rollup-plugin-copy';
 
 const production = !process.env.ROLLUP_WATCH;
 
-const gulp = require('gulp');
-const workboxBuild = require('workbox-build');
-// add "service worker" task here
-const serviceWorker = () => {
-	console.log('init sw workboxBuild');
-	return workboxBuild.injectManifest({
-	  swSrc: 'src/sw.js',
-	  swDest: 'public/sw.js',
-	  globDirectory: 'public',
-	  globPatterns: [
-		'**/*.{html,json,js,css}'
-	  ]
-	}).then(resources => {
-	  console.log(`Injected ${resources.count} resources for precaching, ` +
-		  `totaling ${resources.size} bytes.`);
-	}).catch(err => {
-	  console.log('Uh oh 😬', err);
-	});
-  }
-  gulp.task('service-worker', serviceWorker);
-
-
 export default {
 	input: 'src/main.js',
 	output: {
--- src/main.js
+++ src/main.js
@@ -13,11 +13,34 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
       .then(registration => {
         console.log(`Service Worker registered! Scope: ${registration.scope}`);
+        //registration.showNotification(`Service Worker registered!`);  // if we want to send a push notification to the user
       })
       .catch(err => {
         console.log(`Service Worker registration failed: ${err}`);
+        //err.showNotification(`Service Worker registration failed`);  // if we want to send a notification to the user
       });
   });
 }
+
+const gulp = require('gulp');
+const workboxBuild = require('workbox-build');
+// add "service worker" task here
+const serviceWorker = () => {
+	console.log('init sw workboxBuild');
+	return workboxBuild.injectManifest({
+	  swSrc: 'src/sw.js',
+	  swDest: 'public/sw.js',
+	  globDirectory: 'public',
+	  globPatterns: [
+		'**/*.{html,json,js,css}'
+	  ]
+	}).then(resources => {
+	  console.log(`Injected ${resources.count} resources for precaching, ` +
+		  `totaling ${resources.size} bytes.`);
+	}).catch(err => {
+	  console.log('Uh oh 😬', err);
+	});
+}
+gulp.task('service-worker', serviceWorker);
 
 export default app;
